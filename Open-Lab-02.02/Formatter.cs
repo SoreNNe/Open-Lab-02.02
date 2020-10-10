0cb5e173db7cb9d2e4ffe32c639fcfc97c861087
SoreNNe <71930836+SoreNNe@users.noreply.github.com>
2020-10-10 23:00:28 +0200

    .

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Open_Lab_02._02
 {
     public class Formatter
-    {
+    {        
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            string j = null;
+            for (int i = 0; i < numbers.Length; i++)
+           
+            {
+                double cislo = numbers[i];
+                j = j + Math.Round(cislo, 2).ToString() + " ";
+               
+            }
+            Console.Write(j.Replace(",", ".").TrimEnd());
+           
         }
     }
 }
