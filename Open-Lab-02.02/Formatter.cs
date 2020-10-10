8f769697ed95582576d35a91b0daa8c941c23d09
SoreNNe <71930836+SoreNNe@users.noreply.github.com>
2020-10-10 23:20:12 +0200

    .

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -8,12 +8,10 @@ namespace Open_Lab_02._02
         public void Format(double[] numbers)
         {
             string j = null;
-            for (int i = 0; i < numbers.Length; i++)
-           
+            for (int i = 0; i < numbers.Length; i++)          
             {
                 double cislo = numbers[i];
-                j = j + Math.Round(cislo, 2).ToString() + " ";
-               
+                j = j + Math.Round(cislo, 2).ToString() + " ";              
             }
             Console.Write(j.Replace(",", ".").TrimEnd());
            
